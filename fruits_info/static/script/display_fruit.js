47bb9d3e52893be0f5514252022410284e44d729
FRED KANEZA <95358548+amankanz@users.noreply.github.com>
2024-07-01 17:07:25 +0300

    1st Integration JS
    Integrating JS into python

---
+++ fruits_info/static/script/display_fruit.js
@@ -0,0 +1,11 @@
+const fruit_image = document.getElementById("fruitImage");
+
+window.addEventListener("load", function () {
+  let imageData = this.localStorage.getItem("imageData");
+
+  if (imageData) {
+    fruit_image.src = imageData;
+  } else {
+    alert("No image data found.");
+  }
+});

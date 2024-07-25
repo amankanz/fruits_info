3f0a404989462d7d24fddc9fca029f906cdab008
amunipelumi <amunipelumi@gmail.com>
2024-07-25 15:46:58 +0100

    minor revamp

--- fruits_info/app/agent.py
+++ fruits_info/app/agent.py
@@ -1,6 +1,7 @@
 from groq import Groq
 from os.path import abspath, dirname, join
 import environ
+import json
 
 
 env = environ.Env()
@@ -9,23 +10,40 @@ api_key = env('GROQ_KEY')
 
 def health_benefits(fruit):
         client = Groq(api_key=api_key)
-        prompt = f"I am very curious to know some health benefits of fruits, so give me an example using {fruit}"
-    
-        response = client.chat.completions.create(
-            model="llama3-70b-8192",
-            messages=[
-                # {
-                #     "role": "system",
-                #     "content": "JSON"
-                # },
-                {
-                    "role": "user",
-                    "content": prompt
-                }
-            ],
-            temperature=1,
-            max_tokens=100,
-        )
-
-        benefits = response.choices[0].message.content
-        return benefits
+        schema = {'benefit': 'detailed explanation'}
+        # to prevent a break, i will return none if agent encounters an error.
+        try:
+            response = client.chat.completions.create(
+                model="llama3-70b-8192",
+                messages=[
+                        {
+                            "role": "system",
+                            "content": f"You are a program that returns this kind of JSON schema {schema}"
+                        },
+                        {
+                            "role": "user",
+                            "content": f"Give me the health benefits of a fruit such as {fruit}"
+                        },
+                ],
+                temperature=1,
+                max_tokens=1024,
+                top_p=1,
+                stream=False,
+                response_format={"type": "json_object"},
+                stop=None,
+            )
+            benefits = response.choices[0].message.content
+            benefits = json.loads(benefits)
+            # --------------------------------------------------
+            # print(type(benefits))
+            # num = 0
+            # for key, value in benefits.items():
+            #         num += 1
+            #         print('title: ', key, '\n')
+            #         print('content: ', value)
+            #         print(num)
+            # --------------------------------------------------
+            return benefits
+        
+        except:
+              return None

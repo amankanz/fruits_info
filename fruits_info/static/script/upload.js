0f5a6a3c9439d520f6339c2976be30e2b5883964
FRED KANEZA <95358548+amankanz@users.noreply.github.com>
2024-07-26 14:15:35 +0300

    fix: function disabled functionalities on the camera and upload btns

--- fruits_info/static/script/upload.js
+++ fruits_info/static/script/upload.js
@@ -1,59 +1,18 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const upload_button = document.getElementById("uploadButton");
-//   const file_input = document.getElementById("fileInput");
-
-//   upload_button.addEventListener("click", function () {
-//     file_input.click();
-//   });
-
-//   file_input.addEventListener("change", function (ev) {
-//     const file = ev.target.files[0];
-
-//     if (file) {
-//       const reader = new FileReader();
-//       reader.onload = function (e) {
-//         let imageData = e.target.result;
-//         localStorage.setItem("imageData", imageData);
-
-//         // Show the spinner
-//         document.getElementById("spinner").style.display = "block";
-
-//         fetch(detailspageUrl, {
-//           method: "POST",
-//           headers: {
-//             "Content-Type": "application/json",
-//             "X-CSRFToken": csrftoken,
-//           },
-//           body: JSON.stringify({ imageData: imageData }),
-//         })
-//           .then((response) => {
-//             if (response.ok) {
-//               return response.json();
-//             } else {
-//               throw new Error("Network response was not ok");
-//             }
-//           })
-//           .then((data) => {
-//             // console.log("Success:", data);
-//             window.location.href = detailspageUrl; // Redirect to the details page
-//           })
-//           .catch((error) => {
-//             console.error("Error:", error);
-//           })
-//           .finally(() => {
-//             // Hide the spinner once the data is fetched or an error occurs
-//             // spinner.style.display = "none";
-//             document.getElementById("spinner").style.display = "none";
-//           });
-//       };
-//       reader.readAsDataURL(file);
-//     }
-//   });
-// });
-
 document.addEventListener("DOMContentLoaded", function () {
   const upload_button = document.getElementById("uploadButton");
+  const camera_button = document.getElementById("cameraButton");
+  const spinner = document.getElementById("spinner");
   const file_input = document.getElementById("fileInput");
+
+  function disableButtons() {
+    camera_button.disabled = true;
+    upload_button.disabled = true;
+  }
+
+  function enableButtons() {
+    camera_button.disabled = false;
+    upload_button.disabled = false;
+  }
 
   upload_button.addEventListener("click", function () {
     file_input.click();
@@ -69,7 +28,10 @@ document.addEventListener("DOMContentLoaded", function () {
         localStorage.setItem("imageData", imageData);
 
         // Show the spinner
-        document.getElementById("spinner").style.display = "block";
+        spinner.style.display = "block";
+
+        // Disabled the camera and the upload buttons to prevent additional clicks
+        disableButtons();
 
         fetch(detailspageUrl, {
           method: "POST",
@@ -91,10 +53,14 @@ document.addEventListener("DOMContentLoaded", function () {
           })
           .catch((error) => {
             console.error("Error:", error);
+            // Hide the spinner if an error occurs
+            spinner.style.display = "none";
+            // Re-enable the buttons if an error occurs
+            enableButtons();
           })
           .finally(() => {
             // Hide the spinner once the data is fetched or an error occurs
-            document.getElementById("spinner").style.display = "none";
+            spinner.style.display = "none";
           });
       };
       reader.readAsDataURL(file);
